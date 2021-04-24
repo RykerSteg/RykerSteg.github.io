eee0e44f870f9dccc9fad4782ab66a5c95aeea47
RykerSteg <rykersteglich@gmail.com>
2021-04-24 17:28:24 -0600

    final changes

--- aportal.js
+++ aportal.js
@@ -1,40 +1,4 @@
-/*const daynames = [
-   "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "Julay",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-
-const fulldate = `${dayName}, ${d.get Date()} ${monthName} ${year}`;
-
-document.getElementById("currentDate").textContent = fulldate;
-*/
-const options = {
-    weekday: 'long',
-     day: 'numeric',
-      month: 'long',
-       year: 'numeric'
-    };
-document.getElementById('date').textContent = document.new Date().toLocaleDateString('en-US', options);
+function lastModified(){
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+  document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', options);
+}
