94bfcbc61894a6996c13f10511ff1f7c6f648d83
RykerSteg <rykersteglich@gmail.com>
2021-04-24 00:38:31 -0600

    Added JS 2

--- Assignment Portal/portal.js
+++
@@ -1,6 +0,0 @@
-try{
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById("date").textContent = newDate().toLocalDateString('en-us', options);
-} catch (e) {
-    alert('Error with code or your browser does not support Locale');
-}
