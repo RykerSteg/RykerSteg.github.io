967124af02045ee47b0054149639c0a6d29e01b9
RykerSteg <Rykersteglich@gmail.com>
2021-05-20 19:15:30 -0600

    Lesson 5

---
+++ lesson5/bom.js
@@ -0,0 +1,20 @@
+const list = document.querySelector('.list');
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const deletebutton = document.createElement('button');
+    const listItem = document.createElement('li');
+    const text = document.createElement('span');
+
+    listItem.appendChild(text);
+    text.textContent = input.value;
+    listItem.appendChild(deletebutton);
+    deletebutton.textContent = 'X';
+    list.appendChild(listItem);
+    
+    deletebutton.addEventListener('click', function() {
+        list.removeChild(listItem);
+        input.focus();
+    })
+})
